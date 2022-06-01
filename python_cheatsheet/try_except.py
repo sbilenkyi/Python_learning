7c68fc579595b9375a053ea7824c13a10a0505d8
Serhii <bilenkyis@gmail.com>
2022-06-02 01:26:43 +0200

    practicing

--- python_cheatsheet/try_except.py
+++ python_cheatsheet/try_except.py
@@ -15,4 +15,44 @@ except ZeroDivisionError:
 # except тип_винятку as назва_змінної:
 #     блок коду, коли виникла помилка
 
+predators = ['tiger', 'wolf', 'bear']
+# while True:
+#    value = input('Position [q to quit]? ')
+#    if value == 'q':
+#       break
+#    try:
+#       position = int(value)
+#        print(predators[position])
+#    except IndexError as err:
+#        print('Bad index:', position)
+#    except Exception as other:
+#        print('Something else broke:', other)
 
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("Division by zero!")
+    else:
+        print("Result is", result)
+    finally:
+        print("End of the calculation.")
+
+
+# divide(2, 1)
+# divide(2, 0)
+
+
+class IsNotTitleException(Exception):
+    pass
+
+
+try:
+    rooms = ['Kitchen', 'study', 'Hall', 'Bathroom']
+    for room in rooms:
+        if room.title() != room:
+            raise IsNotTitleException('Fault!')
+
+except IsNotTitleException as exc:
+    print(exc)
